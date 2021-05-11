7eb8a9644914913178e75a53b1bdda2fe92ed231
gavrinjo <drazen.gavric@gmail.com>
2021-05-11 13:21:41 +0200

    copy files script

--- Homework_10_Importing/main.py
+++ Homework_10_Importing/main.py
@@ -1,33 +1,43 @@
 import os
 import shutil as sh
 import argparse
+from pathlib import Path
 
-parser = argparse.ArgumentParser(prog="Test", description="Just a small showcase using argument parser and path")
+parser = argparse.ArgumentParser(prog="Copy", description="Just a small showcase using 'argparse', 'pathlib' and 'shutil' modules for copying files")
 
 # NOTE - Add the arguments
-parser.add_argument("-f", help="File name")
-parser.add_argument("s", metavar="[Source location]", type=str, help="Source file location")
-parser.add_argument("t", metavar="[Target location]", type=str, help="Destination")
+parser.add_argument("s", metavar="[Source]", type=str, help="absolute path and filename or subdirectory and filename. "
+                                                            "Make the path absolute, resolving all symlinks on the way")
+parser.add_argument("t", metavar="[Destination]", type=str, help="location to copy to")
 
 # NOTE - Execute parse_args()
 args = parser.parse_args()
 
-filename = args.f
-source = args.s
-target = args.t
+
+def path_resolve(src):
+
+    operand = [".", "*.*", "*"]
+
+    try:
+        if Path(src).resolve().exists():
+            return Path(src).resolve()
+        else:
+            print("Bad command, invalid path or filename")
+    except OSError:
+        if src.split("\\")[-1] in operand:
+            if Path("/".join(src.split("\\")[:-1])).resolve().exists():
+                return Path("/".join(src.split("\\")[:-1])).resolve()
+            else:
+                print("Bad command, invalid path or filename")
 
 
-def copy(src, tar, file):
-    if file is None:
-        file = "*.*"  # ! This is not good, needs to be made in for loop
-
-    s = os.path.join(src, file)
-    t = tar
-    sh.copy2(s, t)
+def copy(src, dst):
+    if path_resolve(src).is_file():
+        sh.copy2(src, Path(dst).resolve())
+    else:
+        for obj in Path(path_resolve(src)).glob("*.*"):
+            sh.copy2(Path(obj), Path(dst).resolve())
 
 
 if __name__ == '__main__':
-    copy(source, target, filename)
-    print(filename)
-    print(source)
-    print(target)
+    copy(args.s, args.t)
